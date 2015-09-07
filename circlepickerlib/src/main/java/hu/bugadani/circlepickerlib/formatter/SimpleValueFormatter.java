4994390783dde6dbdb1d697d84e00b156ecaa9cb
Dániel Buga <bugadani@gmail.com>
2015-09-07 10:22:59 +0200

    Improve SimpleValueFormatter

--- circlepickerlib/src/main/java/hu/bugadani/circlepickerlib/formatter/SimpleValueFormatter.java
+++ circlepickerlib/src/main/java/hu/bugadani/circlepickerlib/formatter/SimpleValueFormatter.java
@@ -3,9 +3,21 @@ package hu.bugadani.circlepickerlib.formatter;
 public class SimpleValueFormatter implements ValueFormatter
 {
 
+    private final String mFormat;
+
+    public SimpleValueFormatter(String format)
+    {
+        mFormat = format;
+    }
+
+    public SimpleValueFormatter()
+    {
+        this("%.0f");
+    }
+
     @Override
     public String format(double angle)
     {
-        return String.valueOf((int) angle);
+        return String.format(mFormat, angle);
     }
 }
